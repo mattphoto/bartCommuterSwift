cd1504df368897274e696c85aea26629a52af58a
mattphoto <github@mattkim.com>
2015-07-13 16:12:52 -0700

    Initial Commit

---
+++ bartCommuter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bartCommuter
+//
+//  Created by Matt on 7/13/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

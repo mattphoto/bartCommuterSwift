1c1096bfb020be40808505e842746580f788da0c
mattphoto <github@mattkim.com>
2015-07-30 06:54:06 -0700

    added settings

---
+++ bartCommuter/BartSettings.swift
@@ -0,0 +1,13 @@
+//
+//  BartSettings.swift
+//  bartCommuter
+//
+//  Created by Matt on 7/27/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+import UIKit
+
+class BartSettings: NSObject {
+   
+}
